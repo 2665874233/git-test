5f3ab9e4aa8160c89fba862917cac0f329ad80a4
小北 <102281684+2665874233@users.noreply.github.com>
2022-03-27 19:57:21 +0800

    fush

--- src/main/java/com/wkp/git.java
+++ src/main/java/com/wkp/git.java
@@ -6,5 +6,6 @@ public class git {
         System.out.println("hello git的地二个版本数据");
         System.out.println("hello git的地三个版本数据");
         System.out.println("hello git的地push 修改进行push");
+        System.out.println("hello git的地fush 修改进行fush");
     }
 }
