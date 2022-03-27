df5dcffd8eee323547475edaed1a0e8855b1ea22
wkp <wkp@wy.com>
2022-03-27 15:12:04 +0800

    second commit

--- src/main/java/com/wkp/git.java
+++ src/main/java/com/wkp/git.java
@@ -3,5 +3,6 @@ package com.wkp;
 public class git {
     public static void main(String[] args) {
         System.out.println("hello git的数据记录");
+        System.out.println("hello git的地二个版本数据");
     }
 }
