0cc31544dcf0d998873fe0496b58f480d33c43ac
小北 <102281684+2665874233@users.noreply.github.com>
2022-03-28 09:37:14 +0800

    OP

--- src/main/java/com/wkp/git.java
+++ src/main/java/com/wkp/git.java
@@ -10,4 +10,10 @@ public class git {
                 System.out.println("hello git的地push 修改进行push");
         System.out.println("hello git的地fush 修改进行fush");
     }
+    
+    
+     System.out.println("hello git的地二个版本数据");
+        System.out.println("hello git的地三个版本数据");
+        System.out.println("hello git的地push 修改进行push");
+        System.out.println("hello git的地fush 修改进行fush");
 }
